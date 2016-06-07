bc3599371cdc53fb911ebd5225b84a33f9bbceda
polkadotstingray <sakahoshi.contract@gmail.com>
2016-06-07 18:21:02 +0900

    generator walk()

--- chapter7/iterator.py
+++ chapter7/iterator.py
@@ -1,5 +1,3 @@
-import os
-
 i = iter([1, 2, 3])
 # 次の要素を取り出すことと次があるかチェックするだけなので、早い
 print(next(i))
@@ -18,6 +16,7 @@ for line in open("test.txt"):  # tst.txtを行ごとに処理
 # #########  generator  ########## #
 # 今までreturnで処理後に反していたものをyieldを使って途中からバンバン返す
 
+
 def get_primes(x=2):
     while True:
         for i in range(2, x):
@@ -31,18 +30,3 @@ i = get_primes()
 for c in range(10):
     print(next(i))
 
-# ============================================== #
-#                  これがおしゃれ                  #
-# ============================================== #
-
-for dpath, dnames, fnames in os.walk("../"):
-    for fname in fnames:              # os.walkで取ってきたfnamesから順番に探していく
-        if fname.endswith(".py"):     # .pyで終わってるファイル名の物があればTrue
-            print(dpath, ":", fname)  #
-
-"""
-ディレクトリツリー以下のファイル名を、ツリーをトップダウンもしくはボトムアップに走査することで作成します。ディレクトリ top
-を根に持つディレクトリツリーに含まれる、各ディレクトリ (top 自身を含む ) ごとに、
-タプル (dirpath, dirnames, filenames) を yield します。
-"""
-# アンパックを使ってる。walkで調べるディレクトリを指定
